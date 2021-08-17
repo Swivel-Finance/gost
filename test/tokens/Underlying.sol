4bca749c02f9d57e8e39140cd29024953194320e
Janko Simonovic <simonovic86@gmail.com>
2021-08-17 22:47:26 +0200

    Add allowance to `underlying` and compile (#148)
    * add allowance and compile underlying

--- test/tokens/Underlying.sol
+++ test/tokens/Underlying.sol
@@ -2,17 +2,21 @@
 
 /**
   Underlying is a (for now) ERC20 compatible token interface
-
-  NOTE: with the addition of the ZCToken, we may just be able to use its bindings vs maintaining this... TODO
 */
 
 pragma solidity 0.8.4;
 
+// For v2 we only need to read the balance and allowance of underlying
 contract Underlying {
   mapping (address => uint256) public balances;
 
-  // For v1 we only need to read the balance of underlying
+  mapping (address => mapping (address => uint256)) public allowances;
+
   function balanceOf(address o) public view returns (uint256) {
     return balances[o]; // this should not matter to the abi. i think...
+  }
+
+  function allowance(address o, address s) public view returns (uint256) {
+    return allowances[o][s];
   }
 }
