6e5cc3c11820c5e6c4670e7faa52092f32620f67
Julian Traversa <unitedterrorhots@gmail.com>
2021-11-03 13:49:04 -0400

    prevent self notional transfer

--- test/vaulttracker/VaultTracker.sol
+++ test/vaulttracker/VaultTracker.sol
@@ -147,6 +147,7 @@ contract VaultTracker {
   /// @param t Recipient of the amount
   /// @param a Amount to transfer
   function transferNotionalFrom(address f, address t, uint256 a) external onlyAdmin(admin) returns (bool) {
+    require(f != t, 'cannot transfer notional to self');
     Vault memory from = vaults[f];
     Vault memory to = vaults[t];
 
