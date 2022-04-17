993abfc9618c58f78ce54e095a64c3e9ea1a55dc
Julian Traversa <unitedterrorhots@gmail.com>
2022-04-16 22:56:04 -0400

    Euler Adapter + License updates

--- test/vaulttracker/EulerAdapter.sol
+++ test/vaulttracker/EulerAdapter.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.4;
 
-import "./LibFuse.sol";
+import "./IEToken.sol";
 
 contract RariAdapter {
 
     function exchangeRateCurrent(address a) external view returns (uint256){
-        return(LibFuse.viewExchangeRate(CERC20(a)));
+        return(IEToken(a).convertBalanceToUnderlying(1e18));
     }
 }
