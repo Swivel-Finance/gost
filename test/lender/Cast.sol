65936ea255782f3040335fb9e67b991261c70336
robrobbins <rob.robb.ns@gmail.com>
2022-04-27 11:36:59 -0700

    lender work. wax on. wax off.:w

---
+++ test/lender/Cast.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.8.13;
+
+
+library Cast {
+    /// @dev Safely cast an uint256 to an uint128
+    /// @param n the u256 to cast to u128
+    function u128(uint256 n) internal pure returns (uint128) {
+        require (n <= type(uint128).max, ''); // TODO err msgs
+        return uint128(n);
+    }
+}
