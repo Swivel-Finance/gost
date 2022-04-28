ff72aa34f39ce97dc75ee029a56e93e4b76ade0f
Sourabh Marathe <surja795@gmail.com>
2022-04-28 14:27:09 -0400

    mock: yield token functionality

---
+++ test/mocks/YieldToken.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.13;
+
+interface IErc20 {
+	function approve(address, uint256) external returns (bool);
+	function transfer(address, uint256) external returns (bool);
+	function balanceOf(address) external returns (uint256);
+	function transferFrom(address, address, uint256) external returns (bool);
+}
+
+contract YieldToken {
+    uint32 private maturityReturn;
+    IErc20 private baseReturn;
+    uint128 private sellBaseReturn;
+    uint128 private sellBasePreviewReturn;
+
+    // mapping of arguments sent to sellBase. key is the passed in address.
+    mapping (address => uint256) public sellBaseCalled;
+    uint128 public sellBasePreviewCalled;
+
+    function base() external view returns (IErc20) {
+        return baseReturn;
+    }
+
+    function maturity() external view returns (uint32) {
+        return maturityReturn;
+    }
+
+    function sellBaseReturns(uint128 r) external {
+        sellBaseReturn = r;
+    }
+
+    function sellBasePreviewReturns(uint128 r) external {
+        sellBasePreviewReturn = r;
+    }
+
+    function sellBase(address a, uint128 u) external returns (uint128) {
+        sellBaseCalled[a] = u;
+        return sellBaseReturn;
+    }
+
+    function sellBasePreview(uint128 u) external returns (uint128) {
+        sellBasePreviewCalled = u;
+        return sellBasePreviewReturn;
+    }
+}
