3057389a3c79b6d073fe7c875d70c641c38a3bd5
Sourabh Marathe <surja795@gmail.com>
2022-04-28 15:51:40 -0400

    test: basic unit tests for SellBase and SellBasePreview methods

--- pkg/lendertesting/yield_token_test.go
+++ pkg/lendertesting/yield_token_test.go
@@ -5,6 +5,7 @@ import (
 	test "testing"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"github.com/swivel-finance/gost/test/mocks"
 )
@@ -35,6 +36,47 @@ func (s *yieldTokenTestSuite) SetupSuite() {
 			Signer: s.Env.Owner.Opts.Signer,
 		},
 	}
+}
+
+func (s *yieldTokenTestSuite) TestSellBase() {
+	assert := assert.New(s.T())
+	tx, err := s.YieldToken.SellBaseReturns(big.NewInt(1000))
+	assert.NotNil(tx)
+	assert.Nil(err)
+	s.Env.Blockchain.Commit()
+
+	amount := big.NewInt(ONE_ETH)
+	// fake user1 sell base of ONE_ETH
+	tx, err = s.YieldToken.SellBase(
+		s.Env.User1.Opts.From,
+		amount,
+	)
+	assert.NotNil(tx)
+	assert.Nil(err)
+	s.Env.Blockchain.Commit()
+
+	amountSold, err := s.YieldToken.SellBaseCalled(s.Env.User1.Opts.From)
+	assert.Nil(err)
+	assert.Equal(amount, amountSold)
+}
+
+func (s *yieldTokenTestSuite) TestSellBasePreview() {
+	assert := assert.New(s.T())
+	amount := big.NewInt(ONE_ETH)
+	tx, err := s.YieldToken.SellBasePreviewReturns(amount)
+	assert.NotNil(tx)
+	assert.Nil(err)
+	s.Env.Blockchain.Commit()
+
+	tx, err = s.YieldToken.SellBasePreview(amount)
+	assert.NotNil(tx)
+	assert.Nil(err)
+	s.Env.Blockchain.Commit()
+
+	preview, err := s.YieldToken.SellBasePreviewCalled()
+	assert.Nil(err)
+	assert.Equal(amount, preview)
+
 }
 
 func TestYieldTokenSuite(t *test.T) {
