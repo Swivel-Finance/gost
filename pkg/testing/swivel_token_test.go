786cee44516112b0a4e61993da8f52f9decd2864
Sourabh Marathe <surja795@gmail.com>
2022-05-02 16:21:10 -0400

    Complete unit tests for mocked swivel token

--- pkg/testing/swivel_token_test.go
+++ pkg/testing/swivel_token_test.go
@@ -5,9 +5,15 @@ import (
 	test "testing"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"github.com/swivel-finance/gost/test/mocks"
 )
+
+var ORDERS = []mocks.Order{}
+var AMOUNTS = []*big.Int{}
+var COMPONENTS = []mocks.Components{}
 
 type swivelTokenTestSuite struct {
 	suite.Suite
@@ -35,29 +41,78 @@ func (s *swivelTokenTestSuite) SetupSuite() {
 			Signer: s.Env.Owner.Opts.Signer,
 		},
 	}
+
+	ORDERS = []mocks.Order{
+		{
+			Maker:      s.Env.User1.Opts.From,
+			Underlying: common.BigToAddress(big.NewInt(1)),
+			Vault:      true,
+			Exit:       false,
+			Principal:  big.NewInt(11111),
+			Premium:    big.NewInt(22222),
+			Maturity:   big.NewInt(33333),
+			Expiry:     big.NewInt(44444),
+		},
+		{
+			Maker:      s.Env.User2.Opts.From,
+			Underlying: common.BigToAddress(big.NewInt(2)),
+			Vault:      false,
+			Exit:       true,
+			Principal:  big.NewInt(99999),
+			Premium:    big.NewInt(88888),
+			Maturity:   big.NewInt(77777),
+			Expiry:     big.NewInt(66666),
+		},
+	}
+
+	AMOUNTS = []*big.Int{
+		big.NewInt(111),
+		big.NewInt(222),
+	}
+
+	COMPONENTS = []mocks.Components{
+		{
+			V: 100,
+			R: [32]byte{1, 2, 3},
+			S: [32]byte{4, 5, 6},
+		},
+		{
+			V: 200,
+			R: [32]byte{7, 8, 9},
+			S: [32]byte{10, 11, 12},
+		},
+	}
 }
 
-// func (s *swivelTokenTestSuite) TestInitiate() {
-// 	assert := assert.New(s.T())
-// 	tx, err := s.SwivelToken.Initiate(big.NewInt(1000))
-// 	assert.NotNil(tx)
-// 	assert.Nil(err)
-// 	s.Env.Blockchain.Commit()
+func (s *swivelTokenTestSuite) TestInitiate() {
+	assert := assert.New(s.T())
+	tx, err := s.SwivelToken.Initiate(ORDERS, AMOUNTS, COMPONENTS)
+	assert.NotNil(tx)
+	assert.Nil(err)
+	s.Env.Blockchain.Commit()
 
-// 	amount := big.NewInt(ONE_ETH)
-// 	// fake user1 sell base of ONE_ETH
-// 	tx, err = s.SwivelToken.SellBase(
-// 		s.Env.User1.Opts.From,
-// 		amount,
-// 	)
-// 	assert.NotNil(tx)
-// 	assert.Nil(err)
-// 	s.Env.Blockchain.Commit()
+	initiateAmount, err := s.SwivelToken.InitiateCalledAmount(s.Env.User1.Opts.From)
+	assert.Nil(err)
+	assert.Equal(AMOUNTS[0], initiateAmount)
 
-// 	amountSold, err := s.SwivelToken.SellBaseCalled(s.Env.User1.Opts.From)
-// 	assert.Nil(err)
-// 	assert.Equal(amount, amountSold)
-// }
+	initiateAmount, err = s.SwivelToken.InitiateCalledAmount(s.Env.User2.Opts.From)
+	assert.Nil(err)
+	assert.Equal(AMOUNTS[1], initiateAmount)
+}
+
+func (s *swivelTokenTestSuite) TestRedeemZcToken() {
+	addr := common.BigToAddress(big.NewInt(1))
+	amount := big.NewInt(2)
+
+	assert := assert.New(s.T())
+	tx, err := s.SwivelToken.RedeemZcToken(addr, amount, big.NewInt(3))
+	assert.Nil(err)
+	assert.NotNil(tx)
+	s.Env.Blockchain.Commit()
+
+	redeemAmount, err := s.SwivelToken.RedeemZcTokenCalledAmount(addr)
+	assert.Equal(amount, redeemAmount)
+}
 
 func TestSwivelTokenSuite(t *test.T) {
 	suite.Run(t, &swivelTokenTestSuite{})
