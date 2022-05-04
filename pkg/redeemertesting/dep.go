aa821a4a02d3077cf511a960adf1f5a11e6502ba
Sourabh Marathe <surja795@gmail.com>
2022-05-04 16:16:01 -0400

    Add redeemer environment and depedency file

---
+++ pkg/redeemertesting/dep.go
@@ -0,0 +1,63 @@
+package redeemertesting
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/swivel-finance/gost/test/mocks"
+)
+
+type Dep struct {
+	Erc20Address      common.Address
+	Erc20             *mocks.Erc20
+	YieldTokenAddress common.Address
+	YieldToken        *mocks.YieldToken
+	ZcTokenAddress    common.Address
+	ZcToken           *mocks.ZcToken
+	SwivelAddress     common.Address
+	Swivel            *mocks.Swivel
+}
+
+func Deploy(e *Env) (*Dep, error) {
+	// deploy the two mock tokens.
+	ercAddress, _, ercContract, ercErr := mocks.DeployErc20(e.Owner.Opts, e.Blockchain)
+
+	if ercErr != nil {
+		return nil, ercErr
+	}
+
+	e.Blockchain.Commit()
+
+	ytAddress, _, ytContract, ytErr := mocks.DeployYieldToken(e.Owner.Opts, e.Blockchain)
+
+	if ytErr != nil {
+		return nil, ytErr
+	}
+
+	e.Blockchain.Commit()
+
+	zcAddress, _, zcContract, zcErr := mocks.DeployZcToken(e.Owner.Opts, e.Blockchain)
+
+	if zcErr != nil {
+		return nil, zcErr
+	}
+
+	e.Blockchain.Commit()
+
+	swivelAddress, _, swivelContract, swivelErr := mocks.DeploySwivel(e.Owner.Opts, e.Blockchain)
+
+	if swivelErr != nil {
+		return nil, swivelErr
+	}
+
+	e.Blockchain.Commit()
+
+	return &Dep{
+		Erc20Address:      ercAddress,
+		Erc20:             ercContract,
+		YieldTokenAddress: ytAddress,
+		YieldToken:        ytContract,
+		ZcTokenAddress:    zcAddress,
+		ZcToken:           zcContract,
+		SwivelAddress:     swivelAddress,
+		Swivel:            swivelContract,
+	}, nil
+}
