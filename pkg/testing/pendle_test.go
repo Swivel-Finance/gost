ecce05a6f767c9d86c759544abe3290be56f35ea
Sourabh Marathe <surja795@gmail.com>
2022-05-05 10:53:17 -0400

    Add unit test for mocked pendle

--- pkg/testing/pendle_test.go
+++ pkg/testing/pendle_test.go
@@ -1,6 +1,12 @@
 package testing
 
 import (
+	"math/big"
+	test "testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"github.com/swivel-finance/gost/test/mocks"
 )
@@ -10,4 +16,55 @@ type pendleTestSuite struct {
 	Env    *Env
 	Dep    *Dep
 	Pendle *mocks.PendleSession
+}
+
+func (s *pendleTestSuite) SetupSuite() {
+	var err error
+
+	s.Env = NewEnv(big.NewInt(ONE_ETH)) // each of the wallets in the env will begin with this balance
+	s.Dep, err = Deploy(s.Env)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// binding owner to both, kind of why it exists - but could be any of the env wallets
+	s.Pendle = &mocks.PendleSession{
+		Contract: s.Dep.Pendle,
+		CallOpts: bind.CallOpts{From: s.Env.Owner.Opts.From, Pending: false},
+		TransactOpts: bind.TransactOpts{
+			From:   s.Env.Owner.Opts.From,
+			Signer: s.Env.Owner.Opts.Signer,
+		},
+	}
+}
+
+func (s *pendleTestSuite) TestUnderlying() {
+	assert := assert.New(s.T())
+	underlying := common.BigToAddress(big.NewInt(10))
+	tx, err := s.Pendle.UnderlyingReturns(underlying)
+	assert.NotNil(tx)
+	assert.Nil(err)
+	s.Env.Blockchain.Commit()
+
+	pendleUnderlying, err := s.Pendle.Underlying()
+	assert.Nil(err)
+	assert.Equal(underlying, pendleUnderlying)
+}
+
+func (s *pendleTestSuite) TestMaturity() {
+	assert := assert.New(s.T())
+	maturity := big.NewInt(20)
+	tx, err := s.Pendle.MaturityReturns(maturity)
+	assert.NotNil(tx)
+	assert.Nil(err)
+	s.Env.Blockchain.Commit()
+
+	pendleMaturity, err := s.Pendle.Maturity()
+	assert.Nil(err)
+	assert.Equal(maturity, pendleMaturity)
+}
+
+func TestPendleSuite(t *test.T) {
+	suite.Run(t, &pendleTestSuite{})
 }
