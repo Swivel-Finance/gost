9e4b152e2bbd7c093b0990e48b65882098f043e4
Sourabh Marathe <surja795@gmail.com>
2022-05-10 10:40:02 -0400

    Implement the redeem functions

--- test/redeemer/Interfaces.sol
+++ test/redeemer/Interfaces.sol
@@ -11,6 +11,30 @@ interface IErc20 {
 	function transferFrom(address, address, uint256) external returns (bool);
 }
 
+interface IRedeemable {
+	function approve(address, uint256) external returns (bool);
+	function transfer(address, uint256) external returns (bool);
+	function balanceOf(address) external returns (uint256);
+	function transferFrom(address, address, uint256) external returns (bool);
+	function redeem(address, address, uint256) external;
+}
+
+interface IIlluminate {
+  function markets(address, uint256) external returns (address[8] calldata);
+}
+
+interface IPendleRedeemable {
+	function redeem(address, address, uint256, bytes32) external;
+}
+
+interface IPendleRouter {
+	function redeemAfterExpiry(bytes32, address, uint256) external;
+}
+
+interface IRouter {
+	function redeemToBacking(address, uint256, uint256, address) external;
+}
+
 interface ISwivel {
     function redeemZcToken(address u, uint256 m, uint256 a) external returns (bool);
 }
@@ -19,3 +43,6 @@ interface IYield {
     function redeem(address to, uint256 amount) external returns (uint256);
 }
 
+interface ISenseDivider {
+	function redeem(address, uint256, uint256) external;
+}
--- test/redeemer/Redeemer.sol
+++ test/redeemer/Redeemer.sol
@@ -2,6 +2,9 @@
 
 pragma solidity 0.8.13;
 
+import "./Illuminate.sol";
+import "./Interfaces.sol";
+
 contract Redeemer {
 
   // NOTE: the imported interfaces don't need to be named anything other than what they are:
@@ -11,13 +14,21 @@ contract Redeemer {
 
   address public admin;
   address public illuminate;
+  address public pendleRouter;
+  address public tempusRouter;
+  address public apRouter;
+  address public illuminateRouter;
 
   event Redeem(uint8 principal, address indexed underlying, uint256 indexed maturity, uint256 amount);  
 
   /// @param m the deployed Illuminate contract
-  constructor(address m) {
+  constructor(address m, address p, address t, address a, address i) {
     admin = msg.sender;
     illuminate = m; // TODO add an authorized setter for this?
+    pendleRouter = p;
+    tempusRouter = t;
+    apRouter = a;
+    illuminateRouter = i;
   }
   /// @dev redeem method signature for illuminate, tempus, apwine
   /// @param p value of a specific principal according to the Illuminate Principals Enum
@@ -25,8 +36,26 @@ contract Redeemer {
   /// @param m maturity of the market being redeemed
   /// @param o owner / tempus pool / apiwine vault ?
   function redeem(uint8 p, address u, uint256 m, address o) public returns (bool) {
-    // ...
-    return true;
+    IErc20 token = IErc20(IIlluminate(illuminate).markets(u, m)[p]);
+
+    IRouter router;
+    if (p == uint8(Illuminate.Principals.Apwine)) {
+      router = IRouter(apRouter);
+    } else if (p == uint8(Illuminate.Principals.Tempus)) {
+      router = IRouter(tempusRouter);
+    } else {
+      router = IRouter(pendleRouter);
+    }
+
+    uint256 amount = token.balanceOf(illuminate);
+
+    token.transferFrom(illuminate, address(this), amount);
+
+    router.redeemToBacking(o, amount, 0, address(this));
+
+    emit Redeem(p, u, m, amount);
+
+    return (true);
   }
 
   /// @dev redeem method signature for swivel, yield, element, 
@@ -34,7 +63,17 @@ contract Redeemer {
   /// @param u underlying token being redeemed
   /// @param m maturity of the market being redeemed
   function redeem(uint8 p, address u, uint256 m) public returns (bool) {
-    // ...
+    
+    IRedeemable token = IRedeemable(IIlluminate(illuminate).markets(u, m)[p]);
+
+    uint256 amount = token.balanceOf(illuminate);
+
+    token.transferFrom(illuminate, address(this), amount);
+
+    token.redeem(address(this), address(this), amount);
+
+    emit Redeem(p, u, m, amount);
+
     return true;
   }
 
@@ -44,7 +83,17 @@ contract Redeemer {
   /// @param m maturity of the market being redeemed
   /// @param i pendle id ?
   function redeem(uint8 p, address u, uint256 m, bytes32 i) public returns (bool) {
-    // ...
+    IErc20 pendleToken = IErc20(IIlluminate(illuminate).markets(u, m)[p]);
+    IPendleRouter Router = IPendleRouter(pendleRouter);
+
+    uint256 amount = pendleToken.balanceOf(illuminate);
+
+    pendleToken.transferFrom(illuminate, address(this), amount);
+
+    Router.redeemAfterExpiry(i, u, m);
+
+    emit Redeem(p, u, m, amount);
+
     return true;
   }
 
@@ -55,7 +104,16 @@ contract Redeemer {
   /// @param d sense wut (divider?) ?
   /// @param a sense wut (adapter?) ?
   function redeem(uint8 p, address u, uint256 m, address d, address a) public returns (bool) {
-    // ...
+    IErc20 senseToken = IErc20(IIlluminate(illuminate).markets(u, m)[p]);
+
+    uint256 amount = senseToken.balanceOf(address(this));
+
+    senseToken.transferFrom(illuminate, address(this), amount);
+
+    ISenseDivider(d).redeem(a, m, amount);
+
+    emit Redeem(p, u, m, amount);
+
     return true;
   }
 }
