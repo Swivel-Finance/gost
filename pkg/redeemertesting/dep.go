3cf35aa30a0ceada77166321229143f42f77807b
Sourabh Marathe <surja795@gmail.com>
2022-05-11 15:09:49 -0400

    Add tempus and apwine router to dep

--- pkg/redeemertesting/dep.go
+++ pkg/redeemertesting/dep.go
@@ -7,18 +7,26 @@ import (
 )
 
 type Dep struct {
-	Erc20Address      common.Address
-	Erc20             *mocks.Erc20
-	YieldAddress      common.Address
-	Yield             *mocks.Yield
-	ZcTokenAddress    common.Address
-	ZcToken           *mocks.ZcToken
-	SwivelAddress     common.Address
-	Swivel            *mocks.Swivel
-	IlluminateAddress common.Address
-	Illuminate        *mocks.Illuminate
-	RedeemerAddress   common.Address
-	Redeemer          *redeemer.Redeemer
+	Erc20Address       common.Address
+	Erc20              *mocks.Erc20
+	YieldAddress       common.Address
+	Yield              *mocks.Yield
+	ZcTokenAddress     common.Address
+	ZcToken            *mocks.ZcToken
+	SwivelAddress      common.Address
+	Swivel             *mocks.Swivel
+	IlluminateAddress  common.Address
+	Illuminate         *mocks.Illuminate
+	APWineAddress      common.Address
+	APWine             *mocks.APWine
+	APWineTokenAddress common.Address
+	APWineToken        *mocks.Erc20
+	TempusAddress      common.Address
+	Tempus             *mocks.Tempus
+	TempusTokenAddress common.Address
+	TempusToken        *mocks.Erc20
+	RedeemerAddress    common.Address
+	Redeemer           *redeemer.Redeemer
 }
 
 func Deploy(e *Env) (*Dep, error) {
@@ -63,7 +71,39 @@ func Deploy(e *Env) (*Dep, error) {
 
 	e.Blockchain.Commit()
 
-	redeemerAddress, _, redeemerContract, redeemerErr := redeemer.DeployRedeemer(e.Owner.Opts, e.Blockchain, mpAddress)
+	apAddress, _, apContract, apErr := mocks.DeployAPWine(e.Owner.Opts, e.Blockchain)
+
+	if apErr != nil {
+		return nil, apErr
+	}
+
+	e.Blockchain.Commit()
+
+	aptAddress, _, aptContract, aptErr := mocks.DeployErc20(e.Owner.Opts, e.Blockchain)
+
+	if aptErr != nil {
+		return nil, aptErr
+	}
+
+	e.Blockchain.Commit()
+
+	tAddress, _, tContract, tErr := mocks.DeployTempus(e.Owner.Opts, e.Blockchain)
+
+	if tErr != nil {
+		return nil, tErr
+	}
+
+	e.Blockchain.Commit()
+
+	ttAddress, _, ttContract, ttErr := mocks.DeployErc20(e.Owner.Opts, e.Blockchain)
+
+	if ttErr != nil {
+		return nil, ttErr
+	}
+
+	e.Blockchain.Commit()
+
+	redeemerAddress, _, redeemerContract, redeemerErr := redeemer.DeployRedeemer(e.Owner.Opts, e.Blockchain, mpAddress, apAddress, ttAddress)
 
 	if redeemerErr != nil {
 		return nil, redeemerErr
@@ -72,17 +112,25 @@ func Deploy(e *Env) (*Dep, error) {
 	e.Blockchain.Commit()
 
 	return &Dep{
-		Erc20Address:      ercAddress,
-		Erc20:             ercContract,
-		YieldAddress:      ytAddress,
-		Yield:             ytContract,
-		ZcTokenAddress:    zcAddress,
-		ZcToken:           zcContract,
-		SwivelAddress:     swivelAddress,
-		Swivel:            swivelContract,
-		IlluminateAddress: mpAddress,
-		Illuminate:        mpContract,
-		RedeemerAddress:   redeemerAddress,
-		Redeemer:          redeemerContract,
+		Erc20Address:       ercAddress,
+		Erc20:              ercContract,
+		YieldAddress:       ytAddress,
+		Yield:              ytContract,
+		ZcTokenAddress:     zcAddress,
+		ZcToken:            zcContract,
+		SwivelAddress:      swivelAddress,
+		Swivel:             swivelContract,
+		IlluminateAddress:  mpAddress,
+		Illuminate:         mpContract,
+		RedeemerAddress:    redeemerAddress,
+		APWine:             apContract,
+		APWineAddress:      apAddress,
+		APWineToken:        aptContract,
+		APWineTokenAddress: aptAddress,
+		Tempus:             tContract,
+		TempusAddress:      tAddress,
+		TempusToken:        ttContract,
+		TempusTokenAddress: ttAddress,
+		Redeemer:           redeemerContract,
 	}, nil
 }
