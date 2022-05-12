3eb2bda8584728947087e95538104438238b2056
robrobbins <rob.robb.ns@gmail.com>
2022-05-12 16:53:44 -0700

    chore: cleanup the mocked zctoken in illuminate test dir

--- test/illuminate/ZcToken.sol
+++ test/illuminate/ZcToken.sol
@@ -8,20 +8,8 @@
 pragma solidity 0.8.13;
 
 contract ZcToken {
-  // a struct to hold the arguments passed to transferFrom
-  // struct TransferFromArgs {
-    // address to;
-    // uint256 amount;
-  // }
-
   // mapping for arguments passed to approve
   mapping (address => uint256) public approveCalled;
-  // mapping of arguments sent to burn. key is the passed in address.
-  // mapping (address => uint256) public burnCalled;
-  // mapping of arguments sent to mint. key is the passed in address.
-  // mapping (address => uint256) public mintCalled;
-  // mapping of arguments sent to transferFrom. key is passed from address.
-  // mapping (address => TransferFromArgs) public transferFromCalled;
 
   address private underlyingReturn;
   uint256 private maturityReturn;
@@ -29,12 +17,6 @@ contract ZcToken {
   string public name;
   string public symbol;
   uint8 public decimals;
-  // a boolean flag which allows us to dictate the return of burn().
-  // bool private burnReturn;
-  // a boolean flag which allows us to dictate the return of mint().
-  // bool private mintReturn;
-  // a boolean flag which allows us to dictate the return of transferFrom().
-  // bool private transferFromReturn;
 
   /// @param u Underlying
   /// @param m Maturity
@@ -45,13 +27,12 @@ contract ZcToken {
     // we can set the privates in the constructor as well...
     underlyingReturn = u;
     maturityReturn = m;
+    // set approve to return true by default so that createMarket won't revert
+    approveReturn = true;
 
     name = n;
     symbol = s;
     decimals = d;
-
-    // set approve to return true by default so that createMarket won't revert
-    approveReturn = true;
   }
 
   function approve(address s, uint256 a) public returns (bool) {
