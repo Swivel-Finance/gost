24fa98e5bae33e04d3aa2b63a62d00b9013eade1
Sourabh <surja795@gmail.com>
2022-05-20 13:48:15 -0400

    Clean up unused functions from Safe.sol files (#252)

--- test/redeemer/Safe.sol
+++ test/redeemer/Safe.sol
@@ -14,52 +14,6 @@ import {IErc20} from "./Interfaces.sol";
 
 library Safe {
   /// @param e Erc20 token to execute the call with
-  /// @param t To address
-  /// @param a Amount being transferred
-  function approve(IErc20 e, address t, uint256 a) internal {
-    bool result;
-
-    assembly {
-      // Get a pointer to some free memory.
-      let pointer := mload(0x40)
-
-      // Write the abi-encoded calldata to memory piece by piece:
-      mstore(pointer, 0x095ea7b300000000000000000000000000000000000000000000000000000000) // Begin with the function selector.
-      mstore(add(pointer, 4), and(t, 0xffffffffffffffffffffffffffffffffffffffff)) // Mask and append the "to" argument.
-      mstore(add(pointer, 36), a) // Finally append the "amount" argument. No mask as it's a full 32 byte value.
-
-      // Call the token and store if it succeeded or not.
-      // We use 68 because the calldata length is 4 + 32 * 2.
-      result := call(gas(), e, 0, pointer, 68, 0, 0)
-    }
-
-    require(success(result), "approve failed");
-  }
-
-  /// @param e Erc20 token to execute the call with
-  /// @param t To address
-  /// @param a Amount being transferred
-  function transfer(IErc20 e, address t, uint256 a) internal {
-    bool result;
-
-    assembly {
-      // Get a pointer to some free memory.
-      let pointer := mload(0x40)
-
-      // Write the abi-encoded calldata to memory piece by piece:
-      mstore(pointer, 0xa9059cbb00000000000000000000000000000000000000000000000000000000) // Begin with the function selector.
-      mstore(add(pointer, 4), and(t, 0xffffffffffffffffffffffffffffffffffffffff)) // Mask and append the "to" argument.
-      mstore(add(pointer, 36), a) // Finally append the "amount" argument. No mask as it's a full 32 byte value.
-
-      // Call the token and store if it succeeded or not.
-      // We use 68 because the calldata length is 4 + 32 * 2.
-      result := call(gas(), e, 0, pointer, 68, 0, 0)
-    }
-
-    require(success(result), "transfer failed");
-  }
-
-  /// @param e Erc20 token to execute the call with
   /// @param f From address
   /// @param t To address
   /// @param a Amount being transferred
