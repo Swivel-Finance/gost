ac38eb60219f9ad609a99ec3a1b515483d55eea5
Sourabh Marathe <surja795@gmail.com>
2022-06-02 09:48:35 -0400

    Compile with notional protocol in marketplace

--- build/lender/MarketPlace.sol
+++ build/lender/MarketPlace.sol
@@ -6,14 +6,15 @@ library MarketPlace /// @notice Explain to an end user what this does
 /// @dev Explain to a developer any extra details
 /// @param Documents a parameter just like in doxygen (must be followed by parameter name) 
 {
-  enum Principals { 
-    Illuminate, 
-    Swivel, 
-    Yield, 
-    Element, 
-    Pendle, 
-    Tempus, 
-    Sense, 
-    Apwine
+  enum Principals {
+    Illuminate,
+    Swivel,
+    Yield,
+    Element,
+    Pendle,
+    Tempus,
+    Sense,
+    Apwine,
+    Notional
   }
 }
