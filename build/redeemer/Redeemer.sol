8876bb61b90ed698bf9d0a8802fb8fd460b658df
Sourabh Marathe <surja795@gmail.com>
2022-06-07 07:29:57 -0400

    Add principal checks and fix unit tests

--- build/redeemer/Redeemer.sol
+++ build/redeemer/Redeemer.sol
@@ -61,8 +61,8 @@ contract Redeemer {
     // Transfer the underlying token to the redeem contract if it is not illuminate
     if (p != uint8(MarketPlace.Principals.Illuminate)) {
         Safe.transferFrom(IErc20(u), marketPlace, address(this), amount);
-    }
-
+    } 
+    
     if (p == uint8(MarketPlace.Principals.Apwine)) {
         // Redeem the underlying token from APWine to illuminate
         IAPWine(apwineAddr).withdraw(o, amount);
@@ -74,6 +74,8 @@ contract Redeemer {
         IZcToken(principal).burn(o, amount);
         // Transfer the original underlying token back to the user
         Safe.transferFrom(IErc20(u), marketPlace, address(this), amount);
+    } else {
+        revert("Invalid principal");
     }
 
     emit Redeem(0, u, m, amount);
@@ -89,6 +91,14 @@ contract Redeemer {
   function redeem(uint8 p, address u, uint256 m) public returns (bool) {
     // Get the principal token that is being redeemed by the user
     address principal = IMarketPlace(marketPlace).markets(u, m, p);
+
+    // Make sure we have the correct principal
+    if (p != uint8(MarketPlace.Principals.Swivel) &&
+        p != uint8(MarketPlace.Principals.Element) &&
+        p != uint8(MarketPlace.Principals.Yield) &&
+        p != uint8(MarketPlace.Principals.Notional)) {
+        revert("Invalid principal");
+    }
 
     // The amount redeemed should be the balance of the principal token held by the illuminate contract
     uint256 amount = IErc20(principal).balanceOf(marketPlace);
@@ -115,13 +125,16 @@ contract Redeemer {
     return true;
   }
 
-  /// @dev redeem method signature for pendle 
+  /// @notice redeem method signature for pendle 
   /// @param p value of a specific principal according to the Illuminate Principals Enum
   /// @param u underlying token being redeemed
   /// @param m maturity of the market being redeemed
   /// @param i forge id used by pendle to redeem the underlying token
   /// @return bool true if the redemption was successful
   function redeem(uint8 p, address u, uint256 m, bytes32 i) public returns (bool) {
+    // Check the principal is pendle
+    require(p == uint8(MarketPlace.Principals.Pendle));
+
     // Get the principal token that is being redeemed by the user
     IErc20 token = IErc20(IMarketPlace(marketPlace).markets(u, m, p));
 
@@ -143,6 +156,9 @@ contract Redeemer {
   /// @param d sense contract that splits the loan's prinicpal and yield
   /// @param o sense contract that [d] calls into to adapt the underlying to sense
   function redeem(uint8 p, address u, uint256 m, address d, address o) public returns (bool) {
+    // Check the principal is sense
+    require(p == uint8(MarketPlace.Principals.Sense));
+    
     // Get the principal token for the given market
     IErc20 token = IErc20(IMarketPlace(marketPlace).markets(u, m, p));
 
