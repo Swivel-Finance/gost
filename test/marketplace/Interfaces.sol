dea37457cb6daeeb0e5d18aed8d9f5a575402eab
Sourabh Marathe <surja795@gmail.com>
2022-06-10 15:33:09 -0400

    Remove view modifier

--- test/marketplace/Interfaces.sol
+++ test/marketplace/Interfaces.sol
@@ -10,35 +10,23 @@ interface IErc5095 {
     event Redeem(address indexed from, address indexed to, uint256 amount);
     event Mature(uint256 timestamp);
 
-    function maturity() external view returns (uint256);
+    function maturity() external returns (uint256);
 
-    function underlying() external view returns (address);
+    function underlying() external returns (address);
 
-    function maturityRate() external view returns (uint256);
+    function maturityRate() external returns (uint256);
 
-    function convertToUnderlying(uint256 principalAmount)
-        external
-        view
-        returns (uint256);
+    function convertToUnderlying(uint256 principalAmount) external returns (uint256);
 
-    function convertToPrincipal(uint256 underlyingAmount)
-        external
-        view
-        returns (uint256);
+    function convertToPrincipal(uint256 underlyingAmount) external returns (uint256);
 
     function maxRedeem(address owner) external view returns (uint256);
 
-    function previewRedeem(uint256 principalAmount)
-        external
-        view
-        returns (uint256);
+    function previewRedeem(uint256 principalAmount) external returns (uint256);
 
-    function maxWithdraw(address owner) external view returns (uint256);
+    function maxWithdraw(address owner) external returns (uint256);
 
-    function previewWithdraw(uint256 principalAmount)
-        external
-        view
-        returns (uint256);
+    function previewWithdraw(uint256 principalAmount) external returns (uint256);
 
     function withdraw(
         uint256 underlyingAmount,
@@ -54,7 +42,7 @@ interface IErc5095 {
 }
 
 interface IPool {
-    function PT() external view returns (IErc5095);
+    function PT() external returns (IErc5095);
 
     function sellUnderlying(address to, uint128 min) external returns (uint128);
 
@@ -72,17 +60,11 @@ interface IPool {
         uint128 max
     ) external returns (uint128);
 
-    function sellUnderlyingPreview(uint128 underlyingIn)
-        external
-        view
-        returns (uint128);
+    function sellUnderlyingPreview(uint128 underlyingIn) external returns (uint128);
 
-    function buyUnderlyingPreview(uint128 underlyingOut)
-        external
-        view
-        returns (uint128);
+    function buyUnderlyingPreview(uint128 underlyingOut) external returns (uint128);
 
-    function sellPTPreview(uint128 PTIn) external view returns (uint128);
+    function sellPTPreview(uint128 PTIn) external returns (uint128);
 
-    function buyPTPreview(uint128 PTOut) external view returns (uint128);
+    function buyPTPreview(uint128 PTOut) external returns (uint128);
 }
