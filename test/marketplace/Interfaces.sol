f8352a8d188e430af05bfe33fc419673bd6fb9a9
Sourabh Marathe <surja795@gmail.com>
2022-06-10 15:35:54 -0400

    Remove argument names

--- test/marketplace/Interfaces.sol
+++ test/marketplace/Interfaces.sol
@@ -7,8 +7,8 @@ interface IErc20 {
 }
 
 interface IErc5095 {
-    event Redeem(address indexed from, address indexed to, uint256 amount);
-    event Mature(uint256 timestamp);
+    event Redeem(address, address, uint256);
+    event Mature(uint256);
 
     function maturity() external returns (uint256);
 
@@ -16,55 +16,55 @@ interface IErc5095 {
 
     function maturityRate() external returns (uint256);
 
-    function convertToUnderlying(uint256 principalAmount) external returns (uint256);
+    function convertToUnderlying(uint256) external returns (uint256);
 
-    function convertToPrincipal(uint256 underlyingAmount) external returns (uint256);
+    function convertToPrincipal(uint256) external returns (uint256);
 
-    function maxRedeem(address owner) external view returns (uint256);
+    function maxRedeem(address) external returns (uint256);
 
-    function previewRedeem(uint256 principalAmount) external returns (uint256);
+    function previewRedeem(uint256) external returns (uint256);
 
-    function maxWithdraw(address owner) external returns (uint256);
+    function maxWithdraw(address) external returns (uint256);
 
-    function previewWithdraw(uint256 principalAmount) external returns (uint256);
+    function previewWithdraw(uint256) external returns (uint256);
 
     function withdraw(
-        uint256 underlyingAmount,
-        address owner,
-        address receiver
+        uint256,
+        address,
+        address
     ) external returns (uint256);
 
     function redeem(
-        uint256 principalAmount,
-        address owner,
-        address receiver
+        uint256,
+        address,
+        address
     ) external returns (uint256);
 }
 
 interface IPool {
     function PT() external returns (IErc5095);
 
-    function sellUnderlying(address to, uint128 min) external returns (uint128);
+    function sellUnderlying(address, uint128) external returns (uint128);
 
     function buyUnderlying(
-        address to,
-        uint128 underlyingOut,
-        uint128 max
+        address,
+        uint128,
+        uint128
     ) external returns (uint128);
 
     function sellPT(address to, uint128 min) external returns (uint128);
 
     function buyPT(
-        address to,
-        uint128 PTOut,
-        uint128 max
+        address,
+        uint128,
+        uint128
     ) external returns (uint128);
 
-    function sellUnderlyingPreview(uint128 underlyingIn) external returns (uint128);
+    function sellUnderlyingPreview(uint128) external returns (uint128);
 
-    function buyUnderlyingPreview(uint128 underlyingOut) external returns (uint128);
+    function buyUnderlyingPreview(uint128) external returns (uint128);
 
-    function sellPTPreview(uint128 PTIn) external returns (uint128);
+    function sellPTPreview(uint128) external returns (uint128);
 
-    function buyPTPreview(uint128 PTOut) external returns (uint128);
+    function buyPTPreview(uint128) external returns (uint128);
 }
